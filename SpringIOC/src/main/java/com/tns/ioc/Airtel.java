ce5b436889afdebaaeabfc2694ee8b87ee326b27
srujana <srujanar308@gmail.com>
2022-06-22 12:53:35 +0530

    first commit

---
+++ SpringIOC/src/main/java/com/tns/ioc/Airtel.java
@@ -0,0 +1,20 @@
+package com.tns.ioc;
+
+public class Airtel implements Sim {
+	
+public Airtel() {
+		
+		System.out.println("Airtel class Constructor");
+	}
+
+	public void SimType() {
+		System.out.println("Welcome to Airtel");
+		
+	}
+
+	public void Membership() {
+		System.out.println("Your eligible for Airtel Premium Membership");
+		
+	}
+
+}
